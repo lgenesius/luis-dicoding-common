91a04189016b5307655b3d5462c025eac5400fd7
Luis Genesius <luis.huang321@gmail.com>
2021-12-03 12:37:38 +0700

    add resources

--- Sources/Common/String+Localization.swift
+++ Sources/Common/String+Localization.swift
@@ -9,7 +9,8 @@ import Foundation
 
 extension String {
   public func localized() -> String {
-      let bundle = Bundle.main
-      return bundle.localizedString(forKey: self, value: nil, table: nil)
+      return NSLocalizedString(self, bundle: .module, comment: self)
+//      let bundle = Bundle.main
+//      return bundle.localizedString(forKey: self, value: nil, table: nil)
   }
 }
